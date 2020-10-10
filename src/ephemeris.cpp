6bc5987b9dfaa501393674d56789a0da0007510a
Matt Stvan <matt@stvan.net>
2020-10-10 11:24:27 -0400

    Spacing

--- src/ephemeris.cpp
+++ src/ephemeris.cpp
@@ -30,30 +30,38 @@ void Ephemeris::print() {
 
 // Create ASCII ephemeris in STK format (.e)
 std::vector<std::string> Ephemeris::format_stk() {
+  
   // Create the vector of lines and write the header
   std::vector<std::string> lines;
   lines.push_back(std::string{"stk.v.11.0"});
   lines.push_back(std::string{"# WrittenBy Arc"});
   lines.push_back(std::string{"BEGIN Ephemeris"});
+
   // Number of ephemeris points line
   std::stringstream n_points;
   n_points << "NumberOfEphemerisPoints " << states.size();
   lines.push_back(n_points.str());
+
   // Scenario epoch line
   std::stringstream epoch_str;
   epoch_str << "ScenarioEpoch " << epoch.format_fractional("%d %b %Y %H:%M:%S");
   lines.push_back(epoch_str.str());
+
   // Central body line
   std::stringstream body_str;
   body_str << "CentralBody " << central_body.get_name();
   lines.push_back(body_str.str());
+
   // Build the ephemeris points
   lines.push_back("CoordinateSystem ICRF");
   lines.push_back("EphemerisTimePosVel");
+
   // For each state in the state list
   for (ICRF state : states) {
+
     // Calculate time since epoch
     double tplus = state.epoch.difference(epoch);
+
     // Stream t+, rx, ry, rz, vx, vy, vz into each line
     std::stringstream point_line;
     point_line << std::setprecision(14);
@@ -61,9 +69,11 @@ std::vector<std::string> Ephemeris::format_stk() {
                << state.position.y << " " << state.position.z << " "
                << state.velocity.x << " " << state.velocity.y << " "
                << state.velocity.z;
+
     // Add the line to the lines vector
     lines.push_back(point_line.str());
   }
+
   lines.push_back("END Ephemeris");
   return lines;
 }
