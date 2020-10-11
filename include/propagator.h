7697e8696cd610ba57af95f3f5791217dd4d4ad2
Matt Stvan <matt@stvan.net>
2020-10-11 10:01:09 -0400

    Numerical propagator base class

--- include/propagator.h
+++ include/propagator.h
@@ -15,4 +15,18 @@ class Propagator {
   std::vector<ICRF> step(UTCTime start, UTCTime stop, double step);
 };
 
+// Numerical propagator base class
+class NumericalPropagator {
+ public:
+  // State used as the initial state
+  ICRF initial_state;
+  // State used for the initial condition of the next integration step
+  ICRF cache_state;
+  // Number of seconds between steps
+  double step_size;
+
+  // Propagate the inital state to specified epoch
+  ICRF propagate(UTCTime epoch);
+};
+
 #endif
