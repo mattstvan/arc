c8f8e1164c5453e52437e4c48c1d51b95387d2f8
matt <matt@vm1.lan>
2020-10-13 10:10:29 -0400

    Added text file read function, moved to cpp file

--- include/file_io.h
+++ include/file_io.h
@@ -4,18 +4,8 @@
 #include <string>
 #include <vector>
 
-int write_lines_to_file(std::vector<std::string> lines, char filename[]) {
-  std::ofstream file;
-  file.open(filename);
-  if (file.is_open()) {
-    for (std::string line : lines) {
-      file << line << std::endl;
-    }
-    file.close();
-    return 0;
-  } else {
-    return 1;
-  }
-}
+int write_lines_to_file(std::vector<std::string> lines, char filename[]);
+
+std::vector<std::string> read_lines_from_file(char filename[]);
 
 #endif
---
+++ src/file_io.cpp
@@ -0,0 +1,30 @@
+#include <file_io.h>
+
+int write_lines_to_file(std::vector<std::string> lines, char filename[]) {
+  std::ofstream file;
+  file.open(filename);
+  if (file.is_open()) {
+    for (std::string line : lines) {
+      file << line << std::endl;
+    }
+    file.close();
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+std::vector<std::string> read_lines_from_file(char filename[]) {
+  std::ifstream file;
+  file.open(filename);
+  if (file.is_open()) {
+    std::string line;
+    std::vector<std::string> lines {};
+    while (getline(file, line)) {
+      lines.push_back(line);
+    }
+    return lines;
+  } else {
+    return std::vector<std::string> {};
+  }
+}
