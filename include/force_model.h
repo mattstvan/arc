d28a993cb52866fe6021cb8d42683437d5e97afa
Matt Stvan <matt@stvan.net>
2020-10-15 18:02:19 -0400

    Formatting

--- include/force_model.h
+++ include/force_model.h
@@ -1,16 +1,14 @@
 #ifndef FORCE_MODEL_H
 #define FORCE_MODEL_H
-#include <vectors.h>
 #include <cartesian.h>
+#include <vectors.h>
 
 // Acceleration force model
 class ForceModel {
-public:
+ public:
   // Get acceleration force at a given state
   // This base method should be overloaded by derived classes
-  virtual Vector3 acceleration(ICRF) {
-    return Vector3{};
-  };
+  virtual Vector3 acceleration(ICRF state) { return Vector3{}; };
 };
 
 #endif
