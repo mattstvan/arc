eb6ed6364af6f6113b47a0cb455d15e0806dbc2d
matt <matt@vm1.lan>
2020-10-29 10:16:40 -0400

    New integrate using vector operators

--- src/rungekutta4.cpp
+++ src/rungekutta4.cpp
@@ -15,20 +15,22 @@ ICRF RungeKutta4::integrate(ICRF &state, double step) {
   // Take derivatives 
   Vector6 k0 {};
   Vector6 k1 = derivatives(state, 0.0, k0).scale(step);
-  Vector6 w_k1 = k1.scale(1.0/2.0);
+  Vector6 w_k1 = k1 / 2.0;
   Vector6 k2 = derivatives(state, step / 2.0, w_k1).scale(step);
-  Vector6 w_k2 = k2.scale(1.0/2.0);
+  Vector6 w_k2 = k2 / 2.0;
   Vector6 k3 = derivatives(state, step / 2.0, w_k2).scale(step);
   Vector6 k4 = derivatives(state, step, k3).scale(step);
   // Start combined vector and add weighted values
-  w_k2 = k2.scale(2.0);
-  Vector6 total = k1.add(w_k2);
-  Vector6 w_k3 = k3.scale(2.0);
-  total = total.add(w_k3).add(k4).scale(1.0 / 6.0);
+  k2 = k2 * 2;
+  k3 = k3 * 2;
+  Vector6 total = k1 + k2;
+  total = total + k3;
+  total = total + k4;
+  total = total / 6;
   // Combined position/velocity
   Vector6 pos_vel {state.position, state.velocity};
   // Add the total weighted velocity/acceleration vector
-  std::array<Vector3, 2> final_vectors = pos_vel.add(total).split();
+  std::array<Vector3, 2> final_vectors = (pos_vel + total).split();
   // Build the new state and return
   DateTime new_epoch = state.epoch.increment(step);
   return ICRF {state.central_body, new_epoch, final_vectors[0], final_vectors[1]};
