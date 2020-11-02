35a9786c62acd8ab3d0c939cf85880a0e16de51a
matt <matt@vm1.lan>
2020-11-02 13:08:01 -0500

    Swap header/source comments

--- include/vectors.h
+++ include/vectors.h
@@ -18,49 +18,123 @@ public:
   // "Z" or "K" component
   double z;
 
-  // Default constructor
+  /*
+  Default constructor
+
+  Sets all elements to zero
+  */
   Vector3();
 
-  // Constructor using doubles
+  /*
+  Constructor using doubles
+
+  @param x "X" or "I" value
+  @param y "Y" or "J" value
+  @param z "Z" or "K" value
+  */
   Vector3(double x, double y, double z);
 
-  // Calculate the magnitude
+  /*
+  Calculate the magnitude
+
+  @returns Magnitude of this vector
+  */
   double mag();
 
-  // Scale by a scalar value using element-wise multiplication
+  /*
+  Scale by a scalar value using element-wise multiplication
+
+  @param scalar Number by which to multiply each element
+  @returns (vectors::Vector3) scaled vector
+  */
   Vector3 scale(double scalar);
 
-  // Calculate the unit vector
+  /*
+  Calculate the unit vector
+
+  @returns (vectors::Vector3) unit vector
+  */
   Vector3 unit();
 
-  // Add another Vector3 using element-wise addition
+  /*
+  Add another Vector3 using element-wise addition
+
+  @param v Vector to add element-wise
+  @returns (vectors::Vector3) Vector representing the element-wise sum
+  */
   Vector3 add(Vector3& v);
 
-  // Calculate a new Vector3 representing the inverse (negated elements)
+  /*
+  Calculate a new Vector3 representing the inverse (negated elements)
+
+  @returns (vectors::Vector3) The same vector, scaled by -1
+  */
   Vector3 inverse();
 
-  // Distance to another Vector3
+  /*
+  Distance to another Vector3, assuming that
+  both coordinate systems/origins are the same
+
+  @param v The vector from which to calculate distance
+  @returns (double) distance between this and the vector v
+  */
   double distance(Vector3& v);
 
-  // Dot product with another Vector3
+  /*
+  Dot product with another Vector3
+
+  @param v The vector to use for dot product calculation
+  @returns (double) Dot product of this and the vector v
+  */
   double dot(Vector3& v);
 
-  // Cross product with another Vector3
+  /*
+  Cross product with another Vector3
+
+  @param v The vector to use for cross product calculation
+  @returns (vector::Vector3) Cross product of this and the vector v
+  */
   Vector3 cross(Vector3& v);
 
-  // Rotate along the x-axis
+  /*
+  Rotate along the x-axis
+
+  @param theta The angle of rotation
+  @returns (vector::Vector3) the rotated vector
+  */
   Vector3 rot_x(double theta);
 
-  // Rotate along the y-axis
+  /*
+  Rotate along the y-axis
+
+  @param theta The angle of rotation
+  @returns (vector::Vector3) the rotated vector
+  */
   Vector3 rot_y(double theta);
 
-  // Rotate along the z-axis
+  /*
+  Rotate along the z-axis
+
+  @param theta The angle of rotation
+  @returns (vector::Vector3) the rotated vector
+  */
   Vector3 rot_z(double theta);
 
-  // Calculate angle to another Vector3
+  /*
+  Calculate angle to another Vector3
+
+  @param v The vector to which to calculate the angle
+  @returns (double) The angle between this and the vector v
+  */
   double angle(Vector3& v);
 
-  // Change coordinates to relative position from a new origin
+  /*
+  Change coordinates to relative position from a 
+  new origin, assuming both vectors have the same starting origin
+
+  @param origin Vector to use as the new origin
+  @returns (vectors::Vector3) This vector centered around the new origin vector
+  */
   Vector3 change_origin(Vector3& origin);
 };
 
@@ -110,22 +184,55 @@ public:
   // "Z" or "K_2" component
   double z;
 
-  // Default constructor
+  /*
+  Default constructor
+
+  Sets all elements to zero
+  */
   Vector6();
 
-  // Constructor using doubles
+  /*
+  Constructor using doubles
+
+  @param a "A" or "X1" value
+  @param b "B" or "Y1" value
+  @param c "C" or "Z1" value
+  @param x "X" or "X2" value
+  @param y "Y" or "Y2" value
+  @param z "Z" or "Z2" value
+  */
   Vector6(double a, double b, double c, double x, double y, double z);
 
-  // Constructor using two Vector3 instances
+  /*
+  Constructor using two Vector3 instances as {a.x, a.y, a.z, b.x, b.y, b.z}
+
+  @param a First vector
+  @param b Second vector
+  @returns (vectors::Vector6) combined a/b vector
+  */
   Vector6(Vector3& a, Vector3& b);
 
-  // Add another Vector6 using element-wise addition
+  /*
+  Add another Vector6 using element-wise addition
+
+  @param v Vector to add element-wise
+  @returns (vectors::Vector6) Vector representing the element-wise sum
+  */
   Vector6 add(Vector6& v);
 
-  // Scale by a scalar value using element-wise multiplication
+  /*
+  Scale by a scalar value using element-wise multiplication
+
+  @param scalar Number by which to multiply each element
+  @returns (vectors::Vector6) scaled vector
+  */
   Vector6 scale(double scalar);
 
-  // Split the elements into two Vector3 instances
+  /*
+  Split the elements into two Vector3 instances
+
+  @returns (std::array<Vector3, 2>) Array of this vector split by first/last 3 elements
+  */
   std::array<Vector3, 2> split();
 };
 
--- src/vectors.cpp
+++ src/vectors.cpp
@@ -4,43 +4,24 @@
 Three-element vector methods
 */
 
-/*
-Default constructor
-
-Sets all elements to zero
-*/
+// Default constructor
 Vector3::Vector3() {
   this->x = 0.0;
   this->y = 0.0;
   this->z = 0.0;
 }
 
-/*
-Constructor using doubles
-
-@param x "X" or "I" value
-@param y "Y" or "J" value
-@param z "Z" or "K" value
-*/
+// Constructor using doubles
 Vector3::Vector3(double x, double y, double z) {
   this->x = x;
   this->y = y;
   this->z = z;
 }
 
-/*
-Calculate the magnitude
-
-@returns (double) magnitude of this vector
-*/
+// Calculate the magnitude
 double Vector3::mag() { return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2)); }
 
-/*
-Scale by a scalar value using element-wise multiplication
-
-@param scalar Number by which to multiply each element
-@returns (vectors::Vector3) scaled vector
-*/
+// Scale by a scalar value using element-wise multiplication
 Vector3 Vector3::scale(double scalar) {
   double new_x = x * scalar;
   double new_y = y * scalar;
@@ -48,19 +29,10 @@ Vector3 Vector3::scale(double scalar) {
   return Vector3{ new_x, new_y, new_z };
 }
 
-/*
-Calculate the unit vector
-
-@returns (vectors::Vector3) unit vector
-*/
+// Calculate the unit vector
 Vector3 Vector3::unit() { return scale(1.0 / mag()); }
 
-/*
-Add another Vector3 using element-wise addition
-
-@param v Vector to add element-wise
-@returns (vectors::Vector3) Vector representing the element-wise sum
-*/
+// Add another Vector3 using element-wise addition
 Vector3 Vector3::add(Vector3& v) {
   double new_x = x + v.x;
   double new_y = y + v.y;
@@ -68,20 +40,10 @@ Vector3 Vector3::add(Vector3& v) {
   return Vector3{ new_x, new_y, new_z };
 }
 
-/*
-Calculate a new Vector3 representing the inverse (negated elements)
-
-@returns (vectors::Vector3) The same vector, scaled by -1
-*/
+// Calculate a new Vector3 representing the inverse (negated elements)
 Vector3 Vector3::inverse() { return scale(-1.0); }
 
-/*
-Distance to another Vector3, assuming that
-both coordinate systems/origins are the same
-
-@param v The vector from which to calculate distance
-@returns (double) distance between this and the vector v
-*/
+// Distance to another Vector3, assuming that both coordinate systems/origins are the same
 double Vector3::distance(Vector3& v) {
   double dx = x - v.x;
   double dy = y - v.y;
@@ -89,12 +51,7 @@ double Vector3::distance(Vector3& v) {
   return sqrt(pow(dx, 2) + pow(dy, 2) + pow(dz, 2));
 }
 
-/*
-Dot product with another Vector3
-
-@param v The vector to use for dot product calculation
-@returns (double) Dot product of this and the vector v
-*/
+// Dot product with another Vector3
 double Vector3::dot(Vector3& v) {
   double mx = x * v.x;
   double my = y * v.y;
@@ -102,12 +59,7 @@ double Vector3::dot(Vector3& v) {
   return mx + my + mz;
 }
 
-/*
-Cross product with another Vector3
-
-@param v The vector to use for cross product calculation
-@returns (vector::Vector3) Cross product of this and the vector v
-*/
+// Cross product with another Vector3
 Vector3 Vector3::cross(Vector3& v) {
   double new_x = y * v.z - z * v.y;
   double new_y = z * v.x - x * v.z;
@@ -115,12 +67,7 @@ Vector3 Vector3::cross(Vector3& v) {
   return Vector3{ new_x, new_y, new_z };
 }
 
-/*
-Rotate along the x-axis
-
-@param theta The angle of rotation
-@returns (vector::Vector3) the rotated vector
-*/
+// Rotate along the x-axis
 Vector3 Vector3::rot_x(double theta) {
   double cos_t = cos(theta);
   double sin_t = sin(theta);
@@ -130,12 +77,7 @@ Vector3 Vector3::rot_x(double theta) {
   return Vector3{ new_x, new_y, new_z };
 }
 
-/*
-Rotate along the y-axis
-
-@param theta The angle of rotation
-@returns (vector::Vector3) the rotated vector
-*/
+// Rotate along the y-axis
 Vector3 Vector3::rot_y(double theta) {
   double cos_t = cos(theta);
   double sin_t = sin(theta);
@@ -145,12 +87,7 @@ Vector3 Vector3::rot_y(double theta) {
   return Vector3{ new_x, new_y, new_z };
 }
 
-/*
-Rotate along the z-axis
-
-@param theta The angle of rotation
-@returns (vector::Vector3) the rotated vector
-*/
+// Rotate along the z-axis
 Vector3 Vector3::rot_z(double theta) {
   double cos_t = cos(theta);
   double sin_t = sin(theta);
@@ -160,21 +97,11 @@ Vector3 Vector3::rot_z(double theta) {
   return Vector3{ new_x, new_y, new_z };
 }
 
-/*
-Calculate angle to another Vector3
-
-@param v The vector to which to calculate the angle
-@returns (double) The angle between this and the vector v
-*/
+// Calculate angle to another Vector3
 double Vector3::angle(Vector3& v) { return acos(dot(v) / (mag() * v.mag())); }
 
-/*
-Change coordinates to relative position from a
-new origin, assuming both vectors have the same starting origin
 
-@param origin Vector to use as the new origin
-@returns (vectors::Vector3) This vector centered around the new origin vector
-*/
+// Change coordinates to relative position from a new origin, assuming both vectors have the same starting origin
 Vector3 Vector3::change_origin(Vector3& origin) {
   Vector3 delta = origin.inverse();
   return add(delta);
@@ -225,11 +152,6 @@ Vector3 operator % (Vector3& v_1, Vector3& v_2) {
 Six-element vector methods
 */
 
-/*
-Default constructor
-
-Sets all elements to zero
-*/
 Vector6::Vector6() {
   this->a = 0.0;
   this->b = 0.0;
@@ -239,16 +161,6 @@ Vector6::Vector6() {
   this->z = 0.0;
 }
 
-/*
-Constructor using doubles
-
-@param a "A" or "X1" value
-@param b "B" or "Y1" value
-@param c "C" or "Z1" value
-@param x "X" or "X2" value
-@param y "Y" or "Y2" value
-@param z "Z" or "Z2" value
-*/
 Vector6::Vector6(double a, double b, double c, double x, double y, double z) {
   this->a = a;
   this->b = b;
@@ -258,13 +170,6 @@ Vector6::Vector6(double a, double b, double c, double x, double y, double z) {
   this->z = z;
 }
 
-/*
-Constructor using two Vector3 instances as {a.x, a.y, a.z, b.x, b.y, b.z}
-
-@param a First vector
-@param b Second vector
-@returns (vectors::Vector6) combined a/b vector
-*/
 Vector6::Vector6(Vector3& a, Vector3& b) {
   this->a = a.x;
   this->b = a.y;
@@ -274,12 +179,6 @@ Vector6::Vector6(Vector3& a, Vector3& b) {
   this->z = b.z;
 }
 
-/*
-Add another Vector6 using element-wise addition
-
-@param v Vector to add element-wise
-@returns (vectors::Vector6) Vector representing the element-wise sum
-*/
 Vector6 Vector6::add(Vector6& v) {
   double da = a + v.a;
   double db = b + v.b;
@@ -290,12 +189,6 @@ Vector6 Vector6::add(Vector6& v) {
   return Vector6{ da, db, dc, dx, dy, dz };
 }
 
-/*
-Scale by a scalar value using element-wise multiplication
-
-@param scalar Number by which to multiply each element
-@returns (vectors::Vector6) scaled vector
-*/
 Vector6 Vector6::scale(double scalar) {
   double new_a = a * scalar;
   double new_b = b * scalar;
@@ -306,11 +199,6 @@ Vector6 Vector6::scale(double scalar) {
   return Vector6{ new_a, new_b, new_c, new_x, new_y, new_z };
 }
 
-/*
-Split the elements into two Vector3 instances
-
-@returns (std::array<Vector3, 2>) Array of this vector split by first/last 3 elements
-*/
 std::array<Vector3, 2> Vector6::split() {
   return { Vector3{a, b, c}, Vector3{x, y, z} };
 }
