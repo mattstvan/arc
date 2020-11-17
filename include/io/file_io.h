1433e8a55ddd02d54b2f1b8f607736a24fbee355
Matt Stvan <matt@stvan.net>
2020-11-16 21:38:22 -0500

    JSON file read/write

--- include/io/file_io.h
+++ include/io/file_io.h
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <json.h>
 
 /*
 Write the given ASCII lines to file
@@ -18,5 +19,22 @@ Read ASCII lines from an existing file
 @param filename Location in the filesystem of the ASCII file to be read
 */
 std::vector<std::string> read_lines_from_file(const char filename[]);
+
+/*
+Write the given JSON to file
+
+@param json JSON object to write
+@param filename Location in the filesystem at which to write the file
+*/
+void write_lines_to_file(nlohmann::json json, const char filename[]);
+
+/*
+Read JSON from an existing file
+
+@param filename Location in the filesystem of the JSON file to be read
+*/
+nlohmann::json read_json_file(const char filename[]);
+
+
 
 #endif
--- src/io/file_io.cpp
+++ src/io/file_io.cpp
@@ -1,9 +1,13 @@
-#include <file_io.h>
 #include <exceptions.h>
+#include <file_io.h>
+
+#include <iomanip>
+#include <istream>
 #include <sstream>
 
 // Write the given ASCII lines to file
-void write_lines_to_file(std::vector<std::string> &lines, const char filename[]) {
+void write_lines_to_file(std::vector<std::string> &lines,
+                         const char filename[]) {
   std::ofstream file;
   file.open(filename);
   if (file.is_open()) {
@@ -11,10 +15,10 @@ void write_lines_to_file(std::vector<std::string> &lines, const char filename[])
       file << line << std::endl;
     }
     file.close();
-  }
-  else {
+  } else {
     std::stringstream msg;
-    msg << "file_io::write_lines_to_file exception: Unable to write to '" << filename << "'";
+    msg << "file_io::write_lines_to_file exception: Unable to write to '"
+        << filename << "'";
     throw ArcException(msg.str());
   }
 }
@@ -30,10 +34,42 @@ std::vector<std::string> read_lines_from_file(const char filename[]) {
       lines.push_back(line);
     }
     return lines;
-  }
-  else {
+  } else {
     std::stringstream msg;
-    msg << "file_io::read_lines_from_file exception: Unable to open '" << filename << "'";
+    msg << "file_io::read_lines_from_file exception: Unable to open '"
+        << filename << "'";
+    throw ArcException(msg.str());
+  }
+}
+
+// Write the given JSON to file
+void write_json_to_file(nlohmann::json json, const char filename[]) {
+  std::ofstream out_file;
+  out_file.open(filename);
+  if (out_file.is_open()) {
+    out_file << std::setw(4) << json << std::endl;
+  } else {
+    std::stringstream msg;
+    msg << "file_io::write_json_to_file exception: Unable to open '" << filename
+        << "'";
+    throw ArcException(msg.str());
+  }
+}
+
+// Read JSON from an existing file
+nlohmann::json read_json_file(const char filename[]) {
+  // Read file to stream
+  std::ifstream in_file;
+  in_file.open(filename);
+  if (in_file.is_open()) {
+    // Create JSON object
+    nlohmann::json json;
+    in_file >> json;
+    return json;
+  } else {
+    std::stringstream msg;
+    msg << "file_io::read_json_file exception: Unable to open '" << filename
+        << "'";
     throw ArcException(msg.str());
   }
 }
